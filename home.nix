ee28dae520a14e8b022f47b2c11fdc9752a264f3
Kevin Hernandez <kevinhernem@gmail.com>
2025-11-10 03:17:04 +0900

    feat: add git branch to brash alias

--- home.nix
+++ home.nix
@@ -13,7 +13,8 @@
     };
 
     initExtra = ''
-      export PS1='\[\e[38;5;93m\]\u\[\e[0m\] in \[\e[38;5;251m\]\w\[\e[0m\] \\$ '
+      export PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
+      export PS1='\[\e[38;5;93m\]\u\[\e[0m\] in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
     '';
   };
   
