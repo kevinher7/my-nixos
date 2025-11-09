3e112a49fec320df0e8e480d8d75d8fdc5a0f2b6
Kevin Hernandez <kevinhernem@gmail.com>
2025-11-10 03:27:16 +0900

    feat: add ghostty config

--- home.nix
+++ home.nix
@@ -17,6 +17,22 @@
       export PS1='\[\e[38;5;93m\]\u\[\e[0m\] in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
     '';
   };
+
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "Flatland";
+      font-family = "JetBrainsMonoNL Nerd Font Mono";
+      keybind = [
+        "ctrl+c=copy_to_clipboard"
+        "ctrl+v=paste_from_clipboard"
+      ];
+      window-decoration = false;
+      window-padding-balance = true;
+      window-padding-y = 0;
+      window-padding-x = 0;
+    };
+  };
   
   home.packages = with pkgs; [
     bat
