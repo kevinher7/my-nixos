a15213043f6bd5ee934ce40cf88dd48ba7f22320
Kevin Hernandez <kevinhernem@gmail.com>
2025-12-15 12:39:47 +0900

    feat: add settinsg to ly

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,15 @@
     '';
   };
 
-  services.displayManager.ly.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "doom";
+      battery_id = "BAT0";
+      clock = "%c";
+      doom_fire_height = 8;
+    };
+  };
 
   services.picom = {
     enable = true;
