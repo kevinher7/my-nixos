299b26bad9bf0e6287c3d7c317dd81a42d5385c6
Kevin Hernandez <kevinhernem@gmail.com>
2025-12-15 23:52:29 +0900

    fix: directory on nrs

--- home.nix
+++ home.nix
@@ -66,7 +66,7 @@
       enable = true;
       shellAliases = {
         btw = "echo i use nixos btw";
-        nrs = "sudo nixos-rebuild switch --flake ~/config-nixos#beans-btw";
+        nrs = "sudo nixos-rebuild switch --flake ~/nixos-config#beans-btw";
         cdnc = "cd ~/nixos-config";
         cdhm = "cd ~/home-manager-dotfiles";
         za = "zathura --fork";
