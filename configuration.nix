7442125d3cea34208f61d3526b170066a70ac18c
Kevin Hernandez <153143551+kevinher7@users.noreply.github.com>
2025-12-15 20:12:38 +0900

    feat: add ly display manager (#7)
    * feat: add ly as a display manager
    
    * feat: add settinsg to ly

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,16 @@
     '';
   };
 
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "doom";
+      battery_id = "BAT0";
+      clock = "%c";
+      doom_fire_height = 8;
+    };
+  };
+
   services.picom = {
     enable = true;
     backend = "glx";
