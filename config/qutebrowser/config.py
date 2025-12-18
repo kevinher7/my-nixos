9a562afc482df02ddf14ce3bb47a8fae5caa4444
Kevin Hernandez <kevinhernem@gmail.com>
2025-12-18 10:21:02 +0900

    Revert "fix: increase qutebrowser startup speed"
    This reverts commit 331d98f30049013c02b46c90c3138f5a59f4a209.

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1867,9 +1867,6 @@ c.qt.workarounds.locale = False
 ## Type: Bool
 c.qt.workarounds.remove_service_workers = False
 
-# Trying to reduce start up time
-c.qt.workarounds.disable_accessibility = True
-
 ## When/how to show the scrollbar.
 ## Type: String
 ## Valid values:
