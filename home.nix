d6e90839609a522bff62c5544f5990cc3755b3be
Kevin Hernandez <kevinhernem@gmail.com>
2026-01-14 17:37:58 +0900

    feat: handle fonts via stylix + add qutebrowser to targets

--- home.nix
+++ home.nix
@@ -5,6 +5,7 @@
     ./config/nixvim/keymappings.nix
     ./config/nixvim/options.nix
     ./config/nixvim/plugins
+
     ./config/qutebrowser/options.nix
   ];
 
@@ -62,11 +63,34 @@
     image = ./walls/girl-reading-book.png;
 
     polarity = "dark";
+
+    fonts = {
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      sansSerif = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sizes = {
+        applications = 12;
+        desktop = 12;
+        popups = 10;
+      };
+    };
+
     targets = {
       rofi.enable = true;
       ghostty.enable = true;
       btop.enable = true;
       zathura.enable = true;
+      qutebrowser.enable = true;
     };
   };
 
@@ -112,7 +136,7 @@
     ghostty = {
       enable = true;
       settings = {
-        font-family = "JetBrainsMono";
+        # font-family = "JetBrainsMono";
         background-opacity = 0.9;
         keybind = [
           "performable:ctrl+c=copy_to_clipboard"
@@ -135,70 +159,6 @@
 
     qutebrowser = {
       enable = true;
-      extraConfig =
-        let
-          # Access the Stylix colors from the global config
-          colors = config.lib.stylix.colors;
-        in
-        ''
-          # Completion widget
-          c.colors.completion.fg = '#${colors.base05}'
-          c.colors.completion.even.bg = '#${colors.base00}'
-          c.colors.completion.odd.bg = '#${colors.base01}'
-          c.colors.completion.category.fg = '#${colors.base0D}'
-          c.colors.completion.category.bg = '#${colors.base00}'
-          c.colors.completion.category.border.top = '#${colors.base00}'
-          c.colors.completion.category.border.bottom = '#${colors.base00}'
-          c.colors.completion.item.selected.fg = '#${colors.base01}'
-          c.colors.completion.item.selected.bg = '#${colors.base0A}'
-          c.colors.completion.item.selected.border.top = '#${colors.base0A}'
-          c.colors.completion.item.selected.border.bottom = '#${colors.base0A}'
-          c.colors.completion.match.fg = '#${colors.base0B}'
-          c.colors.completion.scrollbar.fg = '#${colors.base05}'
-          c.colors.completion.scrollbar.bg = '#${colors.base00}'
-
-          # Downloads
-          c.colors.downloads.bar.bg = '#${colors.base00}'
-          c.colors.downloads.start.fg = '#${colors.base00}'
-          c.colors.downloads.start.bg = '#${colors.base0D}'
-          c.colors.downloads.stop.fg = '#${colors.base00}'
-          c.colors.downloads.stop.bg = '#${colors.base0C}'
-          c.colors.downloads.error.fg = '#${colors.base08}'
-
-          # Statusbar
-          c.colors.statusbar.normal.fg = '#${colors.base05}'
-          c.colors.statusbar.normal.bg = '#${colors.base00}'
-          c.colors.statusbar.insert.fg = '#${colors.base00}'
-          c.colors.statusbar.insert.bg = '#${colors.base0B}'
-          c.colors.statusbar.passthrough.fg = '#${colors.base00}'
-          c.colors.statusbar.passthrough.bg = '#${colors.base0C}'
-          c.colors.statusbar.command.fg = '#${colors.base05}'
-          c.colors.statusbar.command.bg = '#${colors.base00}'
-          c.colors.statusbar.url.fg = '#${colors.base05}'
-          c.colors.statusbar.url.warn.fg = '#${colors.base0E}'
-          c.colors.statusbar.url.error.fg = '#${colors.base08}'
-          c.colors.statusbar.url.hover.fg = '#${colors.base05}'
-          c.colors.statusbar.url.success.http.fg = '#${colors.base0C}'
-          c.colors.statusbar.url.success.https.fg = '#${colors.base0B}'
-
-          # Tabs
-          c.colors.tabs.bar.bg = '#${colors.base00}'
-          c.colors.tabs.indicator.start = '#${colors.base0D}'
-          c.colors.tabs.indicator.stop = '#${colors.base0C}'
-          c.colors.tabs.indicator.error = '#${colors.base08}'
-          c.colors.tabs.odd.fg = '#${colors.base05}'
-          c.colors.tabs.odd.bg = '#${colors.base01}'
-          c.colors.tabs.even.fg = '#${colors.base05}'
-          c.colors.tabs.even.bg = '#${colors.base00}'
-          c.colors.tabs.selected.odd.fg = '#${colors.base00}'
-          c.colors.tabs.selected.odd.bg = '#${colors.base05}'
-          c.colors.tabs.selected.even.fg = '#${colors.base00}'
-          c.colors.tabs.selected.even.bg = '#${colors.base05}'
-          c.colors.tabs.pinned.odd.fg = '#${colors.base05}'
-          c.colors.tabs.pinned.odd.bg = '#${colors.base0B}'
-          c.colors.tabs.pinned.even.fg = '#${colors.base05}'
-          c.colors.tabs.pinned.even.bg = '#${colors.base0C}'
-        '';
     };
 
     zathura = {
@@ -214,9 +174,7 @@
     source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/config/qtile";
     recursive = true;
   };
-  xdg.configFile."qutebrowser/config.py" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/config/qutebrowser/config.py";
-  };
+
   # xdg.configFile."rquickshare" = {
   #   source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/config/rquickshare";
   #   recursive = true;
