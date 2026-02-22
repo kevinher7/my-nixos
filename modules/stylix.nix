52e138cb99ba94b08a97bd8840f67b236f9f8c94
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-22 22:32:34 +0900

    fix: separat stylix configs

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -2,6 +2,8 @@
 {
   stylix = {
     enable = true;
+    autoEnable = true;
+
     image = ../walls/girl-reading-book.png;
     polarity = "dark";
 
@@ -9,6 +11,26 @@
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
       size = 24;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      sansSerif = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sizes = {
+        applications = 12;
+        desktop = 12;
+        popups = 10;
+      };
     };
   };
 }
