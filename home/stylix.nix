e0fd4d5663b8f13dc1c197aa61cbc629676279db
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-22 22:36:49 +0900

    fix: missing stylix.nix file for home manager

---
+++ home/stylix.nix
@@ -0,0 +1,20 @@
+{ ... }:
+{
+  stylix = {
+    enable = true;
+
+    autoEnable = false; # Disable autoEnable to avoid conflicts with NixOS level
+
+    image = ../walls/girl-reading-book.png;
+    polarity = "dark";
+
+    targets = {
+      rofi.enable = true;
+      ghostty.enable = true;
+      btop.enable = true;
+      zathura.enable = true;
+      qutebrowser.enable = true;
+      nixvim.enable = false;
+    };
+  };
+}
