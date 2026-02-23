3fc8f42d555358b72dfd27c474ec2498c3ad63d4
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-24 01:35:11 +0900

    chore: remove old config files

--- config/nixvim/plugins/indent-blankline.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.nixvim = {
-    plugins = {
-      indent-blankline = {
-        enable = true;
-      };
-    };
-  };
-}
--- modules/system.nix
+++ modules/system.nix
@@ -1,11 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
