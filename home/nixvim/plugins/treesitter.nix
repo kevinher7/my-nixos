093d336c35a1e48dff75b69dd6ee263fb8557a2d
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-27 21:18:06 +0900

    fix: import plugin for treesitter-textobject

--- home/nixvim/plugins/treesitter.nix
+++ home/nixvim/plugins/treesitter.nix
@@ -20,16 +20,21 @@
         settings = {
           highlight.enable = true;
           indent.enable = true;
+        };
+      };
 
-          textobjects = {
-            select = {
-              enable = true;
-              keymaps = {
-                "af" = "@function.outer";
-                "if" = "@function.inner";
-                "ac" = "@class.outer";
-                "ic" = "@class.inner";
-              };
+      treesitter-textobjects = {
+        enable = true;
+
+        settings = {
+          select = {
+            enable = true;
+            lookahead = true;
+            keymaps = {
+              "af" = "@function.outer";
+              "if" = "@function.inner";
+              "ac" = "@class.outer";
+              "ic" = "@class.inner";
             };
           };
         };
