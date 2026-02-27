af08e07189f85f7d46b6404c97355210d63d549d
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-27 22:32:33 +0900

    hotfix: evalate hostname on runtime for nrs alias

--- home/bash.nix
+++ home/bash.nix
@@ -1,20 +1,20 @@
 _:
 {
   programs.bash = {
-      enable = true;
+    enable = true;
 
-      # TODO: Make flake configurable per host (use mkForce or smt)
-      shellAliases = {
-        btw = "echo i use nixos btw";
-        nrs = "sudo nixos-rebuild switch --flake ~/nixos-config#beans-btw";
-        cdnc = "cd ~/nixos-config";
+    # TODO: Make flake configurable per host (use mkForce or smt)
+    shellAliases = {
+      btw = "echo i use nixos btw";
+      nrs = ''sudo nixos-rebuild switch --flake ~/nixos-config#$(hostname)'';
+      cdnc = "cd ~/nixos-config";
 
-        za = "zathura --fork";
-      };
+      za = "zathura --fork";
+    };
 
-      initExtra = ''
-        export PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
-        export PS1='\[\e[38;5;93m\]\u\[\e[0m\] in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
-      '';
+    initExtra = ''
+      export PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
+      export PS1='\[\e[38;5;93m\]\u\[\e[0m\] in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
+    '';
   };
 }
