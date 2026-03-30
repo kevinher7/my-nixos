2612e69026e64f84a56bd491b892cfee89ba93c3
Kevin Hernandez <kevinhernem@gmail.com>
2026-03-30 21:56:29 +0900

    feat: add hosts directory for home manager config

--- home/common/default.nix
+++ home/common/default.nix
@@ -9,6 +9,7 @@
   programs.home-manager.enable = true;
 
   home = {
+    # TODO: Modularize the username?
     username = "kevin";
     homeDirectory = lib.mkForce "/home/kevin";
     stateVersion = "25.05";
---
+++ home/hosts/chromebook.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ../common/
+    ../desktops/qtile
+    ../programs/nixvim
+    ../programs/betterlockscreen.nix
+    ../programs/ghostty.nix
+    ../programs/qutebrowser.nix
+    ../programs/rquickshare.nix
+  ];
+
+  home.packages = with pkgs; [
+    playerctl
+  ];
+}
