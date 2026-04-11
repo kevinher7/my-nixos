432256f82b1dd3daf7232901f0ac79be7045c60e
Kevin Hernandez <kevinhernem@gmail.com>
2026-04-11 16:35:23 +0900

    feat: change colors

--- home/common/bash.nix
+++ home/common/bash.nix
@@ -20,7 +20,7 @@
       export PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
 
       ${if hostname == "server" then ''
-      export PS1='\[\e[38;5;27m\]\u\[\e[0m\]@\H in \[\e[38;5;67m\]\w\[\e[0m\] [\[\e[38;5;208m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
+      export PS1='\u@\[\e[38;5;27m\]\H\[\e[0m\] in \[\e[38;5;67m\]\w\[\e[0m\] [\[\e[38;5;208m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
       '' else ''
       export PS1='\[\e[38;5;93m\]\u\[\e[0m\]$ in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
       ''}
