52ef0b92972acb020e26c3dfcb517b2f710879e6
Kevin Hernandez <kevinhernem@gmail.com>
2026-04-11 15:38:43 +0900

    feat: different colors on bash prompt for server

--- home/common/bash.nix
+++ home/common/bash.nix
@@ -19,7 +19,11 @@
     initExtra = ''
       export PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
 
-      export PS1='\[\e[38;5;93m\]\u\[\e[0m\]${if hostname == "server" then ''@\H'' else ''''} in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
+      ${if hostname == "server" then ''
+      export PS1='\[\e[38;5;27m\]\u\[\e[0m\]@\H in \[\e[38;5;67m\]\w\[\e[0m\] [\[\e[38;5;208m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"}
+      '' else ''
+      export PS1='\[\e[38;5;93m\]\u\[\e[0m\]$ in \[\e[38;5;251m\]\w\[\e[0m\] [\[\e[38;5;196m\]${"$"}{PS1_CMD1}\[\e[0m\]] ${"$"} '
+      ''}
     '';
   };
 }
