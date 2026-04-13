a8c2dbe08a7827dd5e1110ce84198bf5684a4684
Kevin Hernandez <153143551+kevinher7@users.noreply.github.com>
2026-04-14 01:44:44 +0900

    refactor: clean up common modules (#36)
    * chore: remove firefox
    
    * refactor: delete the programs.nix module and move it into chrombook config
    
    * chore: remove firefox envar

--- home/desktops/qtile/default.nix
+++ home/desktops/qtile/default.nix
@@ -1,5 +1,7 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
+  programs.gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
+
   xdg.configFile."qtile" = {
     source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/home/desktops/qtile";
     recursive = true;
--- modules/programs.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  programs = {
-    firefox.enable = true;
-    light.enable = true;
-
-    gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
-  };
-}
